c81820e78b6d87ff24597e8e791190046df056f0
Matt Collier <matt.collier@hotmail.com>
2014-08-18 23:21:49 +0100

    seed data posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create([{
+
+	title: "Title of Post",
+	body: "Body of post",
+	url: "http://mattcollier.io/"
+	
+
+}])
