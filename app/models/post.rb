5aa4423ad0b891be64818b98889d237c46b8b402
Matt Collier <matt.collier@hotmail.com>
2014-08-19 00:44:40 +0100

    added post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ActiveRecord::Base
 	has_attached_file :image, default_url: "missing.png"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	validates_format_of :url, with: URI.regexp
+	validates :url, presence: true
+
+	validates :title, presence: true, uniqueness: true
+
+	validates :body, length: { maximum: 1000, minimum: 20 }
 end
