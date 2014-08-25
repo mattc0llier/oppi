56af26230771c9d27e57631436630560f30570b1
Matt Collier <matt.collier@hotmail.com>
2014-08-25 20:39:01 +0100

    created subjects controller and added images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823103732) do
+ActiveRecord::Schema.define(version: 20140825193504) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 20140823103732) do
     t.string   "title"
     t.text     "body"
     t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  create_table "subjects", force: true do |t|
+    t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image_file_name"
