deaa93070c415d727577a8b98075c3e3b8b95c65
Matt Collier <matt.collier@hotmail.com>
2014-10-08 16:40:34 +1000

    Delete subject id from projects table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008062137) do
+ActiveRecord::Schema.define(version: 20141008063733) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20141008062137) do
   end
 
   create_table "projects", force: true do |t|
-    t.integer  "subject_id"
     t.string   "title"
     t.text     "body"
     t.string   "url"
