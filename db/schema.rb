143e513bba14019767fdd2473575e1cba750323d
Matt Collier <matt.collier@hotmail.com>
2014-10-10 14:05:41 +1000

    Add user_id to projects and users + user index to both

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008063733) do
+ActiveRecord::Schema.define(version: 20141010033451) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141008063733) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "project_id"
+    t.integer  "user_id"
   end
 
   create_table "projects", force: true do |t|
@@ -36,6 +37,7 @@ ActiveRecord::Schema.define(version: 20141008063733) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "taggings", force: true do |t|
