49959ecb575b01728235b6afdb1c76a4e0553594
Matt Collier <matt.collier@hotmail.com>
2014-10-11 18:00:26 +1000

    user model has_many posts, projects relationship

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -17,7 +17,7 @@ class Ability
       can :manage, Asset do |asset|
         asset.assetable.try(:owner) == user
       end
-    end
+  end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
