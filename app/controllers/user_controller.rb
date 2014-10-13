00048865678bc0cae1fba5519d318e68494e9b03
Matt Collier <matt.collier@hotmail.com>
2014-10-13 15:59:57 +1000

    Latest update to site, user -> posts not working

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,19 @@
 class UserController < ApplicationController
+
+def edit
+ 	@user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      flash[:success] = "Your user was updated"
+      redirect_to user_path(@user)
+    else
+      flash[:error] = "Oops somethinig went wrong"
+      render :edit
+    end
+ end
+
 end
