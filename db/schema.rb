c4e25d9c27cb2379748c6d415104ffaa21f6ee89
Matt Collier <matt.collier@hotmail.com>
2014-10-23 18:20:33 +0700

    Playing around with heroku errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016052319) do
+ActiveRecord::Schema.define(version: 20141011062740) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 20141016052319) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "user_id"
-    t.string   "workflow_state"
   end
 
   add_index "projects", ["user_id"], name: "index_projects_on_user_id"
