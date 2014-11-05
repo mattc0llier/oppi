de046201b768286e9e3b637f28e10107fd758029
Matt Collier <matt.collier@hotmail.com>
2014-11-05 19:41:09 +0700

    Got github loading and accepted autorization but now throwing ssl error

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:github]
 
   extend FriendlyId
 	friendly_id :username, use: :slugged
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 root "main#index"
 
- devise_for :users
+ devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
 resources :projects  
 resources :posts
